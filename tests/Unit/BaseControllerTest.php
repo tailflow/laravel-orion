6afec8c455b25e453125146f3434d25da09cb2d0
Aleksei Zarubin <alex@zarubin.co>
2019-10-26 19:01:06 +0200

    feature: add tests placeholders

---
+++ tests/Unit/BaseControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Orion\Http\Controllers\BaseController;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BaseControllerTest
+ *
+ * @coversDefaultClass BaseController
+ */
+class BaseControllerTest extends TestCase
+{
+    /**
+     * @covers ::resourceAbilityMap
+     */
+    public function testExample() {
+        $this->assertTrue(true);
+    }
+}
