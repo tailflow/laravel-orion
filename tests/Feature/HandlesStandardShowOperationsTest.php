aae64968f25ce16a80a840732e4fbee3a7d460d6
Aleksei Zarubin <alex@zarubin.co>
2019-12-20 19:46:26 +0100

    feature: resources show endpoint authorization tests

--- tests/Feature/HandlesStandardShowOperationsTest.php
+++ tests/Feature/HandlesStandardShowOperationsTest.php
@@ -2,19 +2,54 @@
 
 namespace Orion\Tests\Feature;
 
+use Orion\Tests\Fixtures\App\Models\History;
+use Orion\Tests\Fixtures\App\Models\Post;
+use Orion\Tests\Fixtures\App\Models\Supplier;
 use Orion\Tests\Fixtures\App\Models\Tag;
+use Orion\Tests\Fixtures\App\Models\User;
 
 class HandlesStandardShowOperationsTest extends TestCase
 {
     /** @test */
     public function can_get_a_single_resource()
     {
-        $resource = factory(Tag::class)->create();
+        $tag = factory(Tag::class)->create();
 
-        $response = $this->get("/api/tags/{$resource->id}");
+        $response = $this->get("/api/tags/{$tag->id}");
 
-        $response->assertStatus(200);
-        $response->assertJsonStructure(['data']);
-        $response->assertJson(['data' => $resource->toArray()]);
+        $this->assertSuccessfulShowResponse($response);
+        $response->assertJson(['data' => $tag->toArray()]);
+    }
+
+    /** @test */
+    public function can_get_a_list_of_resources_if_disable_authorization_trait_is_applied()
+    {
+        $tag = factory(Tag::class)->create();
+
+        $response = $this->get("/api/tags/{$tag->id}");
+
+        $this->assertSuccessfulShowResponse($response);
+    }
+
+    /** @test */
+    public function cannot_get_a_single_resource_if_no_policy_is_defined_and_disable_authorizatin_trait_is_not_applied()
+    {
+        $supplier = factory(Supplier::class)->create();
+        $history = factory(History::class)->create(['supplier_id' => $supplier]);
+
+        $response = $this->get("/api/history/{$history->id}", ['Accept' => 'application/json']);
+
+        $this->assertUnauthorizedResponse($response);
+    }
+
+    /** @test */
+    public function can_get_a_single_resource_when_allowed_by_a_policy()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->actingAs(factory(User::class)->create(), 'api')->get("/api/posts/{$post->id}");
+
+        $this->assertSuccessfulShowResponse($response);
+        $response->assertJson(['data' => $post->toArray()]);
     }
 }
--- tests/Feature/TestCase.php
+++ tests/Feature/TestCase.php
@@ -39,6 +39,15 @@ abstract class TestCase extends BaseTestCase
     /**
      * @param TestResponse $response
      */
+    protected function assertSuccessfulShowResponse($response)
+    {
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['data']);
+    }
+
+    /**
+     * @param TestResponse $response
+     */
     protected function assertUnauthorizedResponse($response)
     {
         $response->assertStatus(403);
