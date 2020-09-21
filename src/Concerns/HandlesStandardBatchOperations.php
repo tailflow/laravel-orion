1963afa3c0b0d3bb2425235ddaa4b7325f589638
Aleksei Zarubin <alex@zarubin.co>
2020-09-21 18:48:40 +0500

    fix: return type hint of standard batch wrapper methods
    Support\Collection should be used instead of Eloquent\Collection

--- src/Concerns/HandlesStandardBatchOperations.php
+++ src/Concerns/HandlesStandardBatchOperations.php
@@ -4,9 +4,9 @@ namespace Orion\Concerns;
 
 use Exception;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
 use Orion\Http\Requests\Request;
 use Orion\Http\Resources\CollectionResource;
 
