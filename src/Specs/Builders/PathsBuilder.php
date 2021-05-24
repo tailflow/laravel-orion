9de0885ffee64e660b00290505a9a2bf568f3901
Aleksei Zarubin <alex@zarubin.co>
2021-05-24 08:29:25 +0500

    feat(specs): paths - soft deletes query parameters

--- src/Specs/Builders/PathsBuilder.php
+++ src/Specs/Builders/PathsBuilder.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Routing\Route;
 use Illuminate\Routing\Router;
+use Orion\Concerns\InteractsWithSoftDeletes;
 use Orion\Http\Controllers\Controller;
 use Orion\Http\Controllers\RelationController;
 use Orion\Specs\Factories\OperationBuilderFactory;
@@ -18,6 +19,8 @@ use Orion\ValueObjects\Specs\Path;
 
 class PathsBuilder
 {
+    use InteractsWithSoftDeletes;
+
     /** @var ResourcesCacheStore */
     protected $resourcesCacheStore;
 
@@ -67,9 +70,17 @@ class PathsBuilder
                 if (in_array($operationName, ['index', 'search', 'store', 'show', 'update', 'destroy', 'restore'])) {
                     $operationBuilder = $this->resolveStandardOperationBuilder($resource, $operationName, $route);
                 } elseif (in_array($operationName, ['associate', 'dissociate'])) {
-                    $operationBuilder = $this->resolveRelationOneToManyOperationBuilder($resource, $operationName, $route);
+                    $operationBuilder = $this->resolveRelationOneToManyOperationBuilder(
+                        $resource,
+                        $operationName,
+                        $route
+                    );
                 } elseif (in_array($operationName, ['attach', 'detach', 'sync', 'toggle', 'updatePivot'])) {
-                    $operationBuilder = $this->resolveRelationManyToManyOperationBuilder($resource, $operationName, $route);
+                    $operationBuilder = $this->resolveRelationManyToManyOperationBuilder(
+                        $resource,
+                        $operationName,
+                        $route
+                    );
                 } else {
                     continue;
                 }
@@ -98,6 +109,20 @@ class PathsBuilder
      */
     protected function buildParameters(Route $route, string $controllerClass): array
     {
+        $pathParameters = $this->buildPathParameters($route, $controllerClass);
+        $queryParameters = $this->buildQueryParameters($route, $controllerClass);
+
+        return collect($pathParameters)->merge($queryParameters)->toArray();
+    }
+
+    /**
+     * @param Route $route
+     * @param string $controllerClass
+     * @return array
+     * @throws BindingResolutionException
+     */
+    protected function buildPathParameters(Route $route, string $controllerClass): array
+    {
         $parameterNames = $route->parameterNames();
         /** @var Controller $controller */
         $controller = app()->make($controllerClass);
@@ -111,9 +136,30 @@ class PathsBuilder
                     $model = app()->make($controller->resolveResourceModelClass());
                 }
 
-                return $this->buildParameter($model, $parameterName, $route);
+                return $this->buildPathParameter($model, $parameterName, $route);
             }
         )->toArray();
+    }
+
+    protected function buildQueryParameters(Route $route, string $controllerClass): array
+    {
+        /** @var Controller $controller */
+        $controller = app()->make($controllerClass);
+        $softDeletes = $this->softDeletes($controller->resolveResourceModelClass());
+
+        switch ($route->getActionMethod()) {
+            case 'destroy':
+                return $softDeletes ? [$this->buildQueryParameter('boolean', 'force')] : [];
+            case 'index':
+            case 'search':
+            case 'show':
+                return $softDeletes ? [
+                    $this->buildQueryParameter('boolean', 'with_trashed'),
+                    $this->buildQueryParameter('boolean', 'only_trashed')
+                ] : [];
+            default:
+                return [];
+        }
     }
 
     /**
@@ -122,7 +168,7 @@ class PathsBuilder
      * @param Route $route
      * @return array
      */
-    protected function buildParameter(Model $model, string $parameterName, Route $route): array
+    protected function buildPathParameter(Model $model, string $parameterName, Route $route): array
     {
         return [
             'schema' => [
@@ -131,6 +177,18 @@ class PathsBuilder
             'name' => $parameterName,
             'in' => 'path',
             'required' => strpos($route->uri(), "{{$parameterName}?}") === false,
+        ];
+    }
+
+    protected function buildQueryParameter(string $type, string $name): array
+    {
+        return [
+            'schema' => [
+                'type' => $type,
+            ],
+            'name' => $name,
+            'in' => 'query',
+            'required' => false,
         ];
     }
 
@@ -151,9 +209,12 @@ class PathsBuilder
      * @return OperationBuilder
      * @throws BindingResolutionException
      */
-    protected function resolveStandardOperationBuilder(RegisteredResource $resource, string $operation, Route $route): OperationBuilder
-    {
-        $operationClassName = "Orion\\Specs\\Builders\\Operations\\".ucfirst($operation).'OperationBuilder';
+    protected function resolveStandardOperationBuilder(
+        RegisteredResource $resource,
+        string $operation,
+        Route $route
+    ): OperationBuilder {
+        $operationClassName = "Orion\\Specs\\Builders\\Operations\\" . ucfirst($operation) . 'OperationBuilder';
 
         return $this->operationBuilderFactory->make($operationClassName, $resource, $operation, $route);
     }
@@ -165,9 +226,14 @@ class PathsBuilder
      * @return RelationOperationBuilder
      * @throws BindingResolutionException
      */
-    protected function resolveRelationOneToManyOperationBuilder(RegisteredResource $resource, string $operation, Route $route): RelationOperationBuilder
-    {
-        $operationClassName = "Orion\\Specs\\Builders\\Operations\\Relations\\OneToMany\\".ucfirst($operation).'OperationBuilder';
+    protected function resolveRelationOneToManyOperationBuilder(
+        RegisteredResource $resource,
+        string $operation,
+        Route $route
+    ): RelationOperationBuilder {
+        $operationClassName = "Orion\\Specs\\Builders\\Operations\\Relations\\OneToMany\\" . ucfirst(
+                $operation
+            ) . 'OperationBuilder';
 
         return $this->relationOperationBuilderFactory->make($operationClassName, $resource, $operation, $route);
     }
@@ -179,9 +245,14 @@ class PathsBuilder
      * @return RelationOperationBuilder
      * @throws BindingResolutionException
      */
-    protected function resolveRelationManyToManyOperationBuilder(RegisteredResource $resource, string $operation, Route $route): RelationOperationBuilder
-    {
-        $operationClassName = "Orion\\Specs\\Builders\\Operations\\Relations\\ManyToMany\\".ucfirst($operation).'OperationBuilder';
+    protected function resolveRelationManyToManyOperationBuilder(
+        RegisteredResource $resource,
+        string $operation,
+        Route $route
+    ): RelationOperationBuilder {
+        $operationClassName = "Orion\\Specs\\Builders\\Operations\\Relations\\ManyToMany\\" . ucfirst(
+                $operation
+            ) . 'OperationBuilder';
 
         return $this->relationOperationBuilderFactory->make($operationClassName, $resource, $operation, $route);
     }
