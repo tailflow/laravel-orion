f9b5014264c7e0522f4573f52537fc0e10a4be67
Aleksei Zarubin <alex@zarubin.co>
2021-06-05 13:54:27 +0500

    feat(specs): build command

--- src/Commands/BuildSpecs.php
+++ src/Commands/BuildSpecs.php
@@ -4,16 +4,39 @@ declare(strict_types=1);
 
 namespace Orion\Commands;
 
+use File;
 use Illuminate\Console\Command;
+use Orion\Specs\Builders\Builder;
+use Orion\Specs\Formatters\YamlFormatter;
+use Orion\Specs\Parsers\YamlParser;
 
 class BuildSpecs extends Command
 {
-    protected $signature = 'orion:specs {--format=yaml}';
+    protected $signature = 'orion:specs {--path=}';
 
     protected $description = 'Generates API specifications in the given format';
 
-    public function handle()
+    public function handle(Builder $builder, YamlParser $parser, YamlFormatter $formatter): int
     {
-        //
+        if (!$path = $this->option('path')) {
+            $path = base_path('specs/specs.yaml');
+        }
+
+        $existingSpecs = [];
+
+        if (File::exists($path)) {
+            $existingSpecs = $parser->parse(File::get($path));
+        }
+
+        $specs = array_merge_recursive(
+            $existingSpecs,
+            $builder->build()
+        );
+
+        $formattedSpecs = $formatter->format($specs);
+
+        File::put($path, $formattedSpecs);
+
+        return 0;
     }
 }
