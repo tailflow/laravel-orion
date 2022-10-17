d1b5251776697aaa47c6bba0c4c31a016458f3b9
Gautier DELEGLISE <gautier.deleglise@gmail.com>
2022-10-17 23:43:25 +0200

    :recycle: white list query fields extends whitelist fields

--- src/Http/Rules/WhitelistedQueryFields.php
+++ src/Http/Rules/WhitelistedQueryFields.php
@@ -4,22 +4,8 @@ namespace Orion\Http\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-class WhitelistedQueryFields implements Rule
+class WhitelistedQueryFields extends WhitelistedField implements Rule
 {
-    /**
-     * @var array $constraints
-     */
-    protected $constraints;
-
-    /**
-     * ValidField constructor.
-     *
-     * @param $constraints
-     */
-    public function __construct(array $constraints)
-    {
-        $this->constraints = $constraints;
-    }
 
     /**
      * Determine if the validation rule passes.
@@ -31,52 +17,11 @@ class WhitelistedQueryFields implements Rule
     public function passes($_, $fields): bool
     {
         foreach (explode(',', $fields) as $field) {
-            if (in_array('*', $this->constraints, true)) {
-                continue;
-            }
-            if (in_array($field, $this->constraints, true)) {
-                continue;
-            }
-
-            if (strpos($field, '.') === false) {
+            if (!parent::passes($_, $field)) {
                 return false;
             }
-
-            $nestedParamConstraints = array_filter(
-                $this->constraints,
-                function ($paramConstraint) {
-                    return strpos($paramConstraint, '.*') !== false;
-                }
-            );
-
-            foreach ($nestedParamConstraints as $nestedParamConstraint) {
-                if (preg_match($this->convertConstraintToRegex($nestedParamConstraint), $field)) {
-                    continue 2;
-                }
-            }
-
-            return false;
         }
 
         return true;
-    }
-
-    /**
-     * @param string $constraint
-     * @return string
-     */
-    protected function convertConstraintToRegex(string $constraint): string
-    {
-        return '/'.str_replace('.*', '\.(\w+)', $constraint).'/';
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return 'The :input field is not whitelisted.';
     }
 }
