79781d75f026f79d5fbf0cc76135f1e2cf8e1bed
Aleksei Zarubin <12220926+alexzarbn@users.noreply.github.com>
2023-01-19 13:51:02 +0100

    fix(test): sorting by many-to-many relation fields - migrations

--- tests/Fixtures/database/migrations/2023_01_19_123656_create_tags_table.php
+++ tests/Fixtures/database/migrations/2023_01_19_123656_create_tags_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTagsTable extends Migration
 {
     public function up()
     {
